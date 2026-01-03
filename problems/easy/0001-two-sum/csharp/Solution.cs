80320b0f424c3c8ca3fd98faa948e469661b41e8
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2026-01-02 20:35:50 -0700

    1 - Two Sum

---
+++ problems/easy/0001-two-sum/csharp/Solution.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> numToIndex = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (numToIndex.ContainsKey(complement))
+                {
+                    return new int[] { numToIndex[complement], i };
+                }
+                
+                numToIndex[nums[i]] = i;
+            }
+
+            return new int[] { };
+        }
+    }
+}
