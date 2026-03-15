d50c7d0ddfb72f070844277092ee0d64be55aa16
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2026-03-15 08:40:21 -0700

    206. Reverse Linked List Solution

---
+++ problems/easy/0205-isomorphic-strings/typescript/solution.ts
@@ -0,0 +1,22 @@
+export function isIsomorphic(s: string, t: string): boolean {
+  const sToT = new Map<string, string>();
+  const tToS = new Map<string, string>();
+
+  for (let i = 0; i < s.length; i++) {
+    const sChar = s[i];
+    const tChar = t[i];
+
+    if (sToT.has(sChar) && sToT.get(sChar) !== tChar) {
+      return false;
+    }
+
+    if (tToS.has(tChar) && tToS.get(tChar) !== sChar) {
+      return false;
+    }
+
+    sToT.set(sChar, tChar);
+    tToS.set(tChar, sChar);
+  }
+
+  return true;
+}
