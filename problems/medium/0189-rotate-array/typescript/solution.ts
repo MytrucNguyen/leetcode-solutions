b8ebdeda84960a310113c549694069d23158afaa
Mytruc Nguyen <12345678+MytrucNguyen@users.noreply.github.com>
2026-06-03 07:27:41 -0700

    189. Rotate Array Solution

---
+++ problems/medium/0189-rotate-array/typescript/solution.ts
@@ -0,0 +1,15 @@
+export function rotate(nums: number[], k: number): void {
+  k = k % nums.length;
+
+  function reverse(start: number, end: number): void {
+    while (start < end) {
+      [nums[start], nums[end]] = [nums[end], nums[start]];
+      start++;
+      end--;
+    }
+  }
+
+  reverse(0, nums.length - 1);
+  reverse(0, k - 1);
+  reverse(k, nums.length - 1);
+}
