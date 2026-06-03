8bb51afdf13efc3ef9948555dc22a4215a705134
Mytruc Nguyen <12345678+MytrucNguyen@users.noreply.github.com>
2026-06-03 07:27:36 -0700

    100. Same Tree Solution

--- problems/easy/0100-same-tree/typescript/solution.ts
+++ problems/easy/0100-same-tree/typescript/solution.ts
@@ -12,6 +12,17 @@
  * }
  */
 
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
 export function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
   // Base case 1: Both trees are empty
   if (p === null && q === null) {
