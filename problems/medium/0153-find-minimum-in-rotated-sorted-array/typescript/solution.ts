e506da6fbdeaa4e5fe85db50dc34dd62fd50bdc9
Mytruc Nguyen <12345678+MytrucNguyen@users.noreply.github.com>
2026-06-03 07:27:42 -0700

    153. Find Minimum in Rotated Sorted Array

--- problems/medium/0153-find-minimum-in-rotated-sorted-array/typescript/solution.ts
+++ problems/medium/0153-find-minimum-in-rotated-sorted-array/typescript/solution.ts
@@ -1,16 +1,16 @@
 export function findMin(nums: number[]): number {
-    let left = 0;
-    let right = nums.length - 1;
+  let left = 0;
+  let right = nums.length - 1;
 
-    while (left < right) {
-        const mid = Math.floor((left + right) / 2);
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
 
-        if (nums[mid] > nums[right]) {
-            left = mid + 1;
-        } else {
-            right = mid;
-        }
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
     }
+  }
 
-    return nums[left];
+  return nums[left];
 }
