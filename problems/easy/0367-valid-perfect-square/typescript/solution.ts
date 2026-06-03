8eee2e78a63f3bae3d3f5f754de14c1163d4f107
Mytruc Nguyen <12345678+MytrucNguyen@users.noreply.github.com>
2026-06-03 07:27:38 -0700

    367. Valid Perfect Square Solution

---
+++ problems/easy/0367-valid-perfect-square/typescript/solution.ts
@@ -0,0 +1,19 @@
+export function isPerfectSquare(num: number): boolean {
+  let left = 1;
+  let right = num;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const square = mid * mid;
+
+    if (square === num) {
+      return true;
+    } else if (square < num) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return false;
+}
